a05007c5b318ea23ce17301258515e154ae1d35f
tonghuutham <tonghuutham@gmail.com>
2023-06-05 22:35:22 +0700

    style Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,16 +33,6 @@ export default function Footer() {
                 }}
             >
                 <CssBaseline />
-                {/* <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-                    <Typography variant="h2" component="h1" gutterBottom>
-                        Sticky footer
-                    </Typography>
-                    <Typography variant="h5" component="h2" gutterBottom>
-                        {'Pin a footer to the bottom of the viewport.'}
-                        {'The footer will move as the main element of the page grows.'}
-                    </Typography>
-                    <Typography variant="body1">Sticky footer placeholder.</Typography>
-                </Container> */}
 
                 <Box
                     component="footer"
@@ -54,7 +44,7 @@ export default function Footer() {
                             theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
                     }}
                 >
-                    <Container maxWidth="lg">
+                    <Container maxWidth="lg" sx={{ textAlign: 'center' }}>
                         <Typography variant="body1">My sticky footer can be found here.</Typography>
                         <Copyright />
                     </Container>
