8501259c8e9de1439ffbf83d60e4b9577ac6ac17
tonghuutham <tonghuutham@gmail.com>
2023-06-05 15:50:44 +0700

    SignUp

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,3 +1,7 @@
 import Login from '../pages/login/Login';
+import SignUp from '../pages/signUp/SignUp';
 
-export const publicRoutes = [{ path: '/login', element: Login }];
+export const publicRoutes = [
+    { path: '/login', element: Login },
+    { path: '/signup', element: SignUp },
+];
