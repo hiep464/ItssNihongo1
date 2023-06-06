9f6da6b7ec5d179c59f37b449b7afe18d57735b9
hiep464 <luuhiep2309@gmail.com>
2023-06-07 00:21:51 +0700

    feat: add filter

--- src/pages/home/ListNanny.jsx
+++ src/pages/home/ListNanny.jsx
@@ -15,16 +15,163 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import GradeIcon from '@mui/icons-material/Grade';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import IconButton from '@mui/material/IconButton';
+import Checkbox from '@mui/material/Checkbox';
+import Slider from '@mui/material/Slider';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8];
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const marks = [
+    {
+        value: 100000,
+        label: '100,000',
+    },
+    {
+        value: 300000,
+        label: '300,000',
+    },
+    {
+        value: 1000000,
+        label: '1000,000',
+    },
+    {
+        value: 2500000,
+        label: '2,500,000',
+    },
+];
+
+function valuetext(value) {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+// (value) => value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
 export default function ListNanny() {
+    const [filter, setFilter] = React.useState(false);
+
     return (
         <ThemeProvider theme={defaultTheme}>
             <CssBaseline />
+            {
+                filter ? 
+                <Typography
+                    component="div"
+                    sx={{
+                        position: 'fixed',
+                        top: '64px',
+                        right: '0',
+                        bottom: '0',
+                        width: '30vw',
+                        backgroundColor: 'rgba(41, 137, 66, 0.7)',
+                        height: 'calc(100vh - 46px)',
+                        zIndex: '1000',
+                        color: 'white',
+                    }}
+                >
+                    <Typography component="h3">Filter</Typography>
+                    <Typography>
+                        {/* <CheckBox> */}
+                        <Typography>
+                            <Typography component="h4">Language</Typography>
+                            <Typography component="div">
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Japanese
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> English
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Korea
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Chinese
+                            </Typography>
+                        </Typography>
+                        {/* Rating */}
+                        <Typography>
+                            <Typography component="h4">Rating</Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> 5*
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> 4*
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> 3*
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> 2*
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> 1*
+                            </Typography>
+                        </Typography>
+                        {/* Experience */}
+                        <Typography>
+                            <Typography component="h4">Experience</Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Newbie
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Morderate
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Year of experience
+                            </Typography>
+                        </Typography>
+                        {/* Address */}
+                        <Typography>
+                            <Typography component="h4">Address</Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Đống Đa
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Hai Bà Trưng
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Cầu Giấy
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Tây Hồ
+                            </Typography>
+                            <Typography>
+                                <Checkbox {...label} sx={{ height: '18px', width: '18px' }} /> Thanh Xuân
+                            </Typography>
+                        </Typography>
+                    </Typography>
+                    <Typography>
+                        <Slider
+                            aria-label="Always visible"
+                            defaultValue={100000}
+                            getAriaValueText={valuetext}
+                            step={10000}
+                            marks={marks}
+                            valueLabelDisplay="on"
+                            min={100000}
+                            max={2500000}
+                            sx={{width: '80%', marginLeft: '30px'}}
+                        />
+                    </Typography>
+                    <Typography>
+                        <Button variant="contained" sx={{ borderRadius: '20px', padding: '3px 20px' }}>
+                            Apply
+                        </Button>
+                        <Button
+                            onClick={() => {setFilter(false)}}
+                            variant="contained"
+                            sx={{ borderRadius: '20px', padding: '3px 20px', backgroundColor: '#d1d1d1', border: 'none' }}
+                        >
+                            Cancel
+                        </Button>
+                    </Typography>
+                </Typography>
+                : ''
+            }
             <main>
                 {/* Hero unit */}
                 <Box
@@ -34,11 +181,17 @@ export default function ListNanny() {
                         pb: 6,
                     }}
                 >
-                    <Container maxWidth="sm">
-                        <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
+                    <Container maxWidth="sm" sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Typography
+                            textAlign="left"
+                            component="h1"
+                            variant="h2"
+                            align="left"
+                            color="text.primary"
+                            gutterBottom
+                        >
                             List Nanny
                         </Typography>
-
                         {/* <Stack
               sx={{ pt: 4 }}
               direction="row"
@@ -48,6 +201,11 @@ export default function ListNanny() {
               <Button variant="contained">Main call to action</Button>
               <Button variant="outlined">Secondary action</Button>
             </Stack> */}
+                        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                            <IconButton onClick={() => {setFilter(true)}}>
+                                <FilterAltIcon sx={{ width: '48px', height: '48px', color: '#177904' }} />
+                            </IconButton>
+                        </Box>
                     </Container>
                 </Box>
 
