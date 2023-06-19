10c8fbb9ae65fddf0cf40270284778df733de163
hiep464 <luuhiep2309@gmail.com>
2023-06-19 23:03:20 +0700

    refactor: delete footer

--- src/layout/registerLayout/RegisterLayout.jsx
+++ src/layout/registerLayout/RegisterLayout.jsx
@@ -1,12 +1,10 @@
 import HeaderRegister from '../../components/HeaderRegister';
-import Footer from '../../components/Footer';
 
 function RegisterLayout({ children }) {
     return (
         <div>
             <HeaderRegister />
             {children}
-            <Footer />
         </div>
     );
 }
